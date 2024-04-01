f8597098305e197f448fa3ea64f33e4de91ce299
Mark Peng <1210917553@qq.com>
2024-04-01 23:58:31 +0800

    feat: add Clarity analysis

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +11,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script
+        id="clarityLoader"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "lpud1tu7mf");
+          `,
+        }}
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
